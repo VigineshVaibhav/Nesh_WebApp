d72afa57520104184ed74cdfc6986bff50e2535e
Viginesh Vaibhav <vigineshvaibhav@gmail.com>
2019-11-02 13:21:23 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# Nesh_WebApp
+﻿# Nesh Backend Engineer Project Documentation
+### Author: Viginesh Vaibhav
+### Date Created: November 2, 2019
+
+The API for this technical assessment is hosted on PythonAnywhere, at  [viginesh22.pythonanywhere.com](viginesh22.pythonanywhere.com). Here is a list of calls you can make to the API:
+
+## 1. Get Current Summary Information of a City
+
+*Syntax:* ``` viginesh22.pythonanywhere.com/summary/<city>```
+
+*Example:* ```viginesh22.pythonanywhere.com/summary/Houston```
+
+*Output:* A JSON object with the following attributes:
+
+- city: City name
+- temperature: current temperature of the city
+- rainfall: Current rainfall level (in inches)
+- rainfall_probability: Probability of rainfall (between 0 and 1, both inclusive)
+- current_weather: Description of current weather in city ("Clear", "Overcast", etc.)
+- rent: Rent value for city (as of Jan 2017)
+
+## 2. Get Cities within Maximum Rent
+
+*Syntax:* ``` viginesh22.pythonanywhere.com/rent/<max_rent>```
+
+*Example:* ``` viginesh22.pythonanywhere.com/rent/1000```
+
+Here, 'max_rent' is in dollars.
+
+*Output:* An array of JSON objects, each of which has the following attributes:
+
+- city: City name
+- rent: Rent value for city (as of Jan 2017)
+
+## 3. Get Average Rainfall and Temperature for a City on a Specific Month
+
+*Syntax:* ```http://viginesh22.pythonanywhere.com/average/<city>,<mm-yyyy>```
+
+*Example:* ```http://viginesh22.pythonanywhere.com/average/Houston,08-2017```
+
+*Output:* A JSON object with the following attributes:
+
+- city: City name
+- month: Numeric value for the given month (between 1 to 12)
+- year: Given year value
+- avg_rain: City's average rainfall for the given month (in inches)
+- avg_temp: City's average temperature for the given month (in Fahrenheit)
+
+## 4. Get Cities within Maximum Rent and Minimum Temperature for a Given Date
+
+*Syntax:* ```http://viginesh22.pythonanywhere.com/rent/temperature/<max_rent>,<min_temp>,<mm-dd-yyyy>```
+
+*Example:* ```http://viginesh22.pythonanywhere.com/rent/temperature/700,50,01-01-2020```
+
+Here, 'max_rent' is in dollars and 'min_temp' is in Fahrenheit.
+
+*Output:* A JSON object with attribute 'cities', which contains a list of all city names which satisfy the given criteria for maximum rent and minimum temperature.
+
+
+## 5. Get Cities within Maximum Rent and Maximum Rainfall for a Given Date
+
+*Syntax:* ```http://viginesh22.pythonanywhere.com/rent/temperature/<max_rent>,<max_rainfall>,<mm-dd-yyyy>```
+
+*Example:* ```http://viginesh22.pythonanywhere.com/rent/temperature/700,2.00,01-01-2020```
+
+Here, 'max_rent' is in dollars and 'max_rainfall' is in inches.
+
+*Output:* A JSON object with attribute 'cities', which contains a list of all city names which satisfy the given criteria for maximum rent and maximum rainfall.
+
+### Important Note
+For the 4<sup>th</sup> and 5<sup>th</sup> API calls described above, I've limited the number of calls to the Dark Sky API to 100. This is because the free tier only allows for 1000 calls a day. So the result will contain only a subset of all the cities that satisfy the given criteria.
+
+In case you find that the API doesn't work or returns a 500 "Internal Server Error" message, it's most likely because the number of calls to Dark Sky's API has exceeded the daily limit of 1000 calls. In that case, please email me about this and I'll create a new API key for Dark Sky. Alternatively, you could wait for 24 hours to reset the number of calls made to the Dark Sky API, and then try again.
+
+
+#### Thank you for giving me the opportunity to work on this project!
--- nesh_app.py
+++ nesh_app.py
@@ -1,51 +1,101 @@
 from flask import Flask
+import sys
 from flask_restful import Api, Resource, reqparse
 import requests
 import json
 import datetime, time, calendar
 import pandas as pd
+import re
 
 app = Flask(__name__)
 api = Api(app)
 
+darksky_api = "74e920dd4c8aeaf60f36abc812cc677a"
+geocode_api = "AIzaSyCNB2W8Z3zCJHDriis9G1AIOtHJ_h0aheQ"
 
 def get_lat_long(city):
-	response = requests.get(("https://maps.googleapis.com/maps/api/geocode/json?address="+str(city)+"&key=AIzaSyCNB2W8Z3zCJHDriis9G1AIOtHJ_h0aheQ"))
+	response = requests.get(("https://maps.googleapis.com/maps/api/geocode/json?address="+str(city)+"&key="+geocode_api))
 	latitude = response.json()['results'][0]['geometry']['location']['lat']
 	longitude = response.json()['results'][0]['geometry']['location']['lng']
 	return (latitude,longitude)
 
+class Summary(Resource):
 
-class Average_Rainfall(Resource):
-
-	def get(self, city, month, year):
+	def get(self, city):
 		lat_long = get_lat_long(city)
 		lat = str(lat_long[0])
 		long = str(lat_long[1])
-		month = int(month)
-		year = int(year)
+		current_time = datetime.datetime.now()
+		unix_time = str(int(time.mktime(current_time.timetuple())))
+		
+		path = "./price.csv"
+		rent_df = pd.read_csv(path)
+
+		weather_data = requests.get("https://api.darksky.net/forecast/"+darksky_api+"/"+lat+","+long+","+unix_time)
+		weather_json = weather_data.json()
+		current_weather = weather_json.get('currently')
+
+		summary = {}
+		summary['city']=city
+
+		if current_weather!=None:
+			summary['temperature'] = current_weather.get('temperature')
+			summary['rainfall'] = float(current_weather.get('precipIntensity'))
+			summary['rainfall_probability'] = float(current_weather.get('precipProbability'))
+			summary['current_weather']=current_weather.get('summary')
+			summary['rent']='N/A'
+
+			for index, row in rent_df.iterrows():
+				if str(row['City'])==city:
+					summary['rent']=float(row['January 2017'])
+					break
+
+		return json.dumps(summary), 200
+
+
+class Average(Resource):
+
+	def get(self, city, date):
+		lat_long = get_lat_long(city)
+		lat = str(lat_long[0])
+		long = str(lat_long[1])
+		month = int(date.split("-")[0])
+		year = int(date.split("-")[1])
 		num_days = calendar.monthrange(year, month)[1]
 		days = [datetime.date(year, month, day) for day in range(1, num_days+1)]
 
-		rain = 0
-		avg_rain = 0
+		rain = 0.00
+		temperature = 0.00
+		avg_rain = 0.00
+		avg_temp = 0.00
 
 
 		for day in days:
 			unix_time = str(int(time.mktime(day.timetuple())))
 			
-			weather_data = requests.get("https://api.darksky.net/forecast/f50cf8c0dbdcd4d9e0e4948585b20362/"+lat+","+long+","+unix_time)
-			print(weather_data.json())
+			weather_data = requests.get("https://api.darksky.net/forecast/"+darksky_api+"/"+lat+","+long+","+unix_time)
+
 			precipIntensity = weather_data.json()['currently']['precipIntensity']
-			
+			temperature += float(weather_data.json()['currently']['temperature'])
+
 			if precipIntensity!=0:
 				precipType = weather_data.json()['currently']['precipType']
 				if precipType=="rain" or precipType=="sleet":
-					rain+=precipIntensity
+					rain+=(float(precipIntensity)*24.00)
 
 		avg_rain = (rain/num_days)*1.000
-		
-		return "Average rainfall in " + str(city) + " on " + str(month) + "/" + str(year) + " is: " + str(avg_rain) + " (in inches)", 200
+		avg_temp = (temperature/num_days)*1.000
+
+		json_obj = {}
+
+		json_obj['city']=city
+		json_obj['month']=month
+		json_obj['year']=year
+		json_obj['avg_rain']=avg_rain
+		json_obj['avg_temp']=avg_temp
+
+		return json.dumps(json_obj)
+
 
 class Rent(Resource):
 
@@ -56,28 +106,132 @@ class Rent(Resource):
 
 		rent = int(rent)
 		i = 0
-		result_df = pd.DataFrame(columns=['City','Rent (As of Jan 2017)'])
+		result_df = pd.DataFrame(columns=['City','Rent'])
 
 		for index, row in rent_df.iterrows():
-			if row['January 2017']<=rent:
-				new = pd.DataFrame({"City":row['City'], "Rent (As of Jan 2017)": row['January 2017']}, index=[i])
+			if row['January 2017']<rent:
+				new = pd.DataFrame({"City":row['City'], "Rent": row['January 2017']}, index=[i])
 				i = i+1
 				result_df = result_df.append(new)
 
-		# data = {}
 
-		# for index, row in result_df.iterrows():
-		# 	data['City']=str(row['City'])
-		# 	data['Rent (As of Jan 2017)'] = str(row['Rent (As of Jan 2017)'])
+		return result_df.to_json(orient='records'), 200
 
-		return json.dumps(result_df.to_json(orient='records').replace("\'","")), 200
+class Rent_Temperature(Resource):
+
+	def get(self, rent, temp, date):
+
+		temp = float(temp)
+		rent = float(rent)
+
+		forecasted_temp = None
+		forecasted_weather = None
+
+		date = re.sub('[-.:]', '/', date)
+		date_obj = datetime.datetime.strptime(date, '%m/%d/%Y')
+		unix_time = str(int(time.mktime(date_obj.timetuple())))
+
+		path = "./price.csv"
+		rent_df = pd.read_csv(path)
+
+		i = 0
+
+		cities=[]
+		result_cities = []
+
+		for index, row in rent_df.iterrows():
+			if row['January 2017']<rent:
+				cities.append(str(row['City']))
+
+		for city in cities:
+
+			#Limit the number of calls to Dark Sky API to a maximum of 100.
+			i += 1
+			if i>100:
+				break
+
+			lat_long = get_lat_long(city)
+			lat = str(lat_long[0])
+			long = str(lat_long[1])
+
+			weather_data = requests.get("https://api.darksky.net/forecast/"+darksky_api+"/"+lat+","+long+","+unix_time)
+			forecasted_weather = weather_data.json().get('currently')
+			if forecasted_weather!=None:
+				forecasted_temp = forecasted_weather.get('temperature')
 
 
-api.add_resource(Average_Rainfall, "/rainfall/<string:city>,<string:month>,<string:year>")
+			if forecasted_temp!=None:
+				if forecasted_temp>=temp:
+					result_cities.append(city)
+
+		result = {}
+		result['cities'] = result_cities
+		print (len(cities), len(result_cities), file=sys.stdout)
+
+		return json.dumps(result)
+
+class Rent_Rainfall(Resource):
+
+	def get(self, rent, rainfall, date):
+
+		rainfall = float(rainfall)
+		rent = float(rent)
+
+		forecasted_rainfall = 0.00
+		forecasted_weather = None
+
+		date = re.sub('[-.:]', '/', date)
+		date_obj = datetime.datetime.strptime(date, '%m/%d/%Y')
+		unix_time = str(int(time.mktime(date_obj.timetuple())))
+
+		path = "./price.csv"
+		rent_df = pd.read_csv(path)
+
+		i = 0
+
+		cities=[]
+		result_cities = []
+
+		for index, row in rent_df.iterrows():
+			if row['January 2017']<rent:
+				cities.append(str(row['City']))
+
+		for city in cities:
+
+			#Limit the number of calls to Dark Sky API to a maximum of 100.
+			i += 1
+			if i>100:
+				break
+
+			lat_long = get_lat_long(city)
+			lat = str(lat_long[0])
+			long = str(lat_long[1])
+
+			weather_data = requests.get("https://api.darksky.net/forecast/"+darksky_api+"/"+lat+","+long+","+unix_time)
+			forecasted_weather = weather_data.json().get('currently')
+
+			if forecasted_weather!=None:
+				forecasted_rainfall = float(forecasted_weather.get('precipIntensity'))*24.00
+
+
+			if forecasted_rainfall!=0:
+				if forecasted_rainfall<=rainfall:
+					result_cities.append(city)
+
+
+		result = {}
+		result['cities'] = result_cities
+
+		return json.dumps(result)
+
+api.add_resource(Summary, "/summary/<string:city>")
+api.add_resource(Average, "/average/<string:city>,<string:date>")
 api.add_resource(Rent, "/rent/<string:rent>")
+api.add_resource(Rent_Temperature, "/rent/temperature/<string:rent>,<string:temp>,<string:date>")
+api.add_resource(Rent_Rainfall, "/rent/rainfall/<string:rent>,<string:rainfall>,<string:date>")
 
 if __name__=="__main__":
-	app.run(debug=True)
+	app.run(debug=False)
 
 
 
